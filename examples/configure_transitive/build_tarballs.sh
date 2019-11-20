d6ac0816fd92172e305deb90d058002892f0efc2
Simon Newton <sjn@uber.com>
2019-11-20 07:08:29 -0800

    CLean up.

--- examples/configure_transitive/build_tarballs.sh
+++ examples/configure_transitive/build_tarballs.sh
@@ -12,18 +12,12 @@ function build_lib() {
   # Builds & creates the dist tarball for the given lib.
   # Make
   local lib="$1"
-  local inst_dir="$2"
-  shift 2;
+  shift;
 
   pushd .
   cd "$DIR/$lib"
   autoreconf -fi
   ./configure $@
-
-  if [ x"${inst_dir}" != x ]; then
-    make
-    make install DESTDIR=$inst_dir
-  fi
   make dist
 
   tarball=$(ls -t $lib*.tar.gz| head -n1)
@@ -31,10 +25,6 @@ function build_lib() {
   popd
 }
 
-instdir=$(mktemp -d)
-build_lib liblength $instdir
-
-libarea_inst=$(mktemp -d)
-build_lib libarea $instdir CFLAGS=-I$instdir/usr/local/include/ LDFLAGS=-L$instdir/usr/local/lib
-
-build_lib libvolume "" CFLAGS=-I$instdir/usr/local/include/ LDFLAGS=-L$instdir/usr/local/lib
+build_lib liblength ""
+build_lib libarea --without-liblength
+build_lib libvolume --without-libarea
